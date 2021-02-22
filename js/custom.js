3fb64a06afd60ba09dec860834f5ded9f87a3018
I-FDesign <ifdesing.contacto@gmail.com>
2021-02-22 18:17:14 -0300

    Home logos slider

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,29 @@ function seeAllProjects(e) {
     })
 }
 
+let actualLogosScreen = 0;
+let goingTo = 'next';
+function logosSlider() {
+    setInterval(() => {
+        if((actualLogosScreen + 1) > 2) {
+            actualLogosScreen--;
+            goingTo = 'back';
+        } else if(goingTo == 'next') {
+            actualLogosScreen++;
+        } else if(actualLogosScreen == 0) {
+            goingTo = 'next';
+            actualLogosScreen++;
+        } else {
+            goingTo = 'back';
+            actualLogosScreen--;
+        }
+        
+        $('.logos-home .active').removeClass('active');
+        const nextScreen = $('.logos-home .screen-inner')[actualLogosScreen];
+        $(nextScreen).addClass('active');
+    }, 5000)
+}
+
 $(document).ready(() => {
     $('.media-project').on('click', (e) => {
         const target = e.target;
@@ -20,4 +43,8 @@ $(document).ready(() => {
 
         $(parent).find('a').click();
     })
+
+    if($('.logos-home').length > 0 && window.innerWidth > 767) {
+        logosSlider();
+    }
 })
