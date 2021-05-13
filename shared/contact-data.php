92e85c1dde96ce167fdb628e0ccacc87fdede707
I-FDesign <ifdesing.contacto@gmail.com>
2021-05-13 10:48:54 -0300

    Footer improved

---
+++ shared/contact-data.php
@@ -0,0 +1,6 @@
+<ul>
+    <li>Dirección : Av. del Libertador 88 - piso 2<br>Vicente Lopez, Buenos Aires, Argentina.<br><br></li>
+    <li>Teléfono : (+54 11) 5199-1401</li>
+    <li>Envíanos tu consulta a : info@introarquitectura.com.ar</li>
+    <li>Envíanos tu CV a : cv@introarquitectura.com.ar</li>
+</ul>
